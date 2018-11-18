07f67b992d03b2ca74a26810fefe346d4006e429
Inna Ivchenko <i_lincoln_a@list.ru>
2018-11-18 18:30:14 +0200

    added gulp group &  compiled import.css

---
+++ hw_4/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp')
+    sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('scss/import.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+  
+});
+
+
